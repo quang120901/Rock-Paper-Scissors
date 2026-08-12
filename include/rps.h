ac358ec3d59ba49ac0e3ea7439f60e054dc5f295
quang120901 <nguyennhatquang963@gmail.com>
2026-08-12 21:24:37 +0700

    update header file

--- include/rps.h
+++ include/rps.h
@@ -3,8 +3,44 @@
 
 #include <Arduino.h>
 
-class rps {
+class rps
+{
+private:
+    String solution[3][2] = {
+        {"rock", "scissors"},
+        {"paper", "rock"},
+        {"scissors", "paper"}};
 
+    struct scores
+    {
+        String match;
+        int score;
+    };
+
+    int previousScore;
+
+public:
+    enum states
+    {
+        playing,
+        waitForUserInput,
+        endE
+    };
+
+    String chosenInput[3] = {"rock", "paper", "scissors"};
+
+    scores GameScore[3]
+    {
+        {"won", 0},
+            {"lost", 0},
+            {"tied", 0}
+    };
+
+    void allLEDsOff();
+    void turnAllLEDsOn();
+    void checkGameplay();
+    void checkUserWin(String user, String bot);
+    void resetGame();
 };
 
 #endif
