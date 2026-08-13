b3909e2ba1829801122f70154c81af30b93b7f75
quang120901 <nguyennhatquang963@gmail.com>
2026-08-13 15:11:04 +0700

    adding demo game

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,13 @@
 #include <Arduino.h>
+#include "RPS.h"
 
-// put function declarations here:
-int myFunction(int, int);
+RPS Game = RPS();
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Serial.begin(115200);
+    Game.checkUserWin("paper","rock");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
