8e6cfecfd8967ee2b99d99f8c900cfa7c314c5fb
quang120901 <nguyennhatquang963@gmail.com>
2026-08-28 16:20:06 +0700

    adding gameplay RPS.cpp

--- src/RPS.cpp
+++ src/RPS.cpp
@@ -1,6 +1,26 @@
 #include <Arduino.h>
 #include "RPS.h"
 
+void RPS::checkGameplay() {
+    if(GameScore[0].score >= 2 || GameScore[1].score >= 2) {
+        state = end;
+    } else {
+        state = waitForUserInput;
+    }
+
+    Serial.print("result");
+    Serial.print("Won:");
+    Serial.print(GameScore[0].score);
+    Serial.print("Lost:");
+    Serial.print(GameScore[1].score);
+    Serial.print("Tied:");
+    Serial.print(GameScore[2].score);
+    Serial.println("---------------");
+
+
+
+}
+
 void RPS::checkUserWin(String user, String bot)
 {
     if (user == bot)
